5e5f99b80f880b388c310e22677529fd18243cba
yohanesokta <yohanesoktayt72@gmail.com>
2024-02-11 13:32:31 +0700

    Fix : Right Char

---
+++ src/components/RoomChat/Message.jsx
@@ -0,0 +1,44 @@
+import "@/components/Styles/RoomChat/message.scss";
+import Image from "next/image"
+
+const Left = ({ data }) => {
+    return (<>
+        <div className="field">
+            <div className={"message-field"}>
+                <div className="profile">
+                    <Image src="/foto.webp" alt="[]" width={100} height={100} />
+                </div>
+                <div className="message-text">
+                    <p>{data?.text}</p>
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+const Right = ({ data }) => {
+    return (<>
+        <div className="field">
+            <div className="gap-right"></div>
+            <div className={"message-field right"}>
+                <div className="message-text">
+                    <p>{data?.text}</p>
+                </div>
+                <div className="profile">
+                    <Image src="/foto.webp" alt="[]" width={100} height={100} />
+                </div>
+            </div>
+        </div>
+    </>)
+}
+
+const Message = ({ fromMe, data }) => {
+    return (
+        <>
+            {(fromMe) ? <Right data={data} /> : <Left data={data} />}
+        </>
+
+    )
+}
+
+export default Message
--- src/components/RoomChat/RoomChat.jsx
+++ src/components/RoomChat/RoomChat.jsx
@@ -1,14 +1,29 @@
+'use client'
+
 import Navigation from "./Navigation"
+import Message from "./Message"
 import "@/components/Styles/RoomChat/page.scss"
+
+
+
 const RoomChat = () => {
+
+
     return (
         <div className="RoomChat-container">
             <Navigation />
             <div className="container">
-
                 <div className="chat">
-                    <div className="message">
-                        <p></p>
+                    <div className="message" id="message-field">
+                        <Message data={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fuga dicta iste suscipit aliquid aut voluptatibus saepe natus soluta, nobis ducimus ipsum? Hic est possimus quia labore molestiae culpa autem facere magnam placeat ipsa! Mollitia dignissimos voluptates architecto soluta ad." }} />
+                        <Message fromMe={true} data={{ text: "Lorem Test 10" }} />
+                        <Message fromMe={true} data={{ text: "Lorem Test 11" }} />
+                        <Message fromMe={true} data={{ text: "Lorem Test 12" }} />
+                        <Message data={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.  Mollitia dignissimos voluptates architecto soluta ad." }} />
+                        <Message data={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.  Mollitia dignissimos voluptates architecto soluta ad." }} />
+                        <Message fromMe={true} data={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.  Mollitia dignissimos voluptates architecto soluta ad." }} />
+                        <Message fromMe={true} data={{ text: "Lorem Test 11" }} />
+                        <Message fromMe={true} data={{ text: "Lorem Test 12" }} />
                     </div>
                     <form action="">
                         <div className="input-message">
